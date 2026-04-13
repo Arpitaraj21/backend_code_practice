3391bc0e469831605235461aaee741f4bcaeea97
Arpita Raj <singh.arpitaraj2001@gmail.com>
2026-04-13 20:16:54 +0530

    [API] done the cors setup

--- PRACTICE-SET/User-Registration-System/server/index.js
+++ PRACTICE-SET/User-Registration-System/server/index.js
@@ -1,10 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import connectDb from './connection.js';
+import cors from 'cors';
+
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT;
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+
+
+app.use(express.json());
 
 connectDb();
 
