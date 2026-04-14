e18c1824f87a25191eb14e2ae92a8c4d267daac6
arpita0212 <arpita@pipra.solutions>
2026-04-14 12:38:26 +0530

    [API] added controller for login

--- PRACTICE-SET/User-Registration-System/server/controllers/user.js
+++ PRACTICE-SET/User-Registration-System/server/controllers/user.js
@@ -1,4 +1,3 @@
-import express from "express";
 import bcrypt, { hash } from "bcrypt";
 import { User } from "../models/Users";
 import dotenv from "dotenv";
@@ -22,10 +21,8 @@ const handleSignup = async (req, res) => {
 
     const hashPassword = await bcrypt.hash(password, 12);
 
-    const token = await jwt.sign({
-      name,
-      role,
-      secretKey,
+    const token = await jwt.sign({ name, role }, secretKey, {
+      expiresIn: "1hr",
     });
 
     const newUser = await User.createOne({
@@ -36,21 +33,69 @@ const handleSignup = async (req, res) => {
       profileImage,
     });
 
-    return res.status(201).json(
-      {
-        message: "User created successfully!",
-        user: newUser,
-        token,
-      },
-      { expiresIn: "1h" },
-    );
+    return res.status(201).json({
+      message: "User created successfully!",
+      user: newUser,
+      token,
+      sucess: true,
+    });
   } catch (error) {
     console.log("signup error", error);
     return res.json({
       status: 500,
       message: "Internal Server Error!",
+      sucess: false,
     });
   }
 };
 
-export default handleSignup;
+const handleLogin = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password) {
+      return res.status(401).json({
+        message: "Email and Password is required!",
+        success: false,
+      });
+    }
+
+    const isUserExisting = await User.findOne({ email });
+
+    if (!isUserExisting) {
+      return res.status(400).json({
+        message: "No user available",
+        sucess: false,
+      });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(
+      password,
+      isUserExisting.password,
+    );
+
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+        success: false,
+      });
+    }
+
+    const token = jwt.sign({ id: user._id, role: user.role }, secretKey, {
+      expiresIn: "1hr",
+    });
+
+    return res.status(200).json({
+      message: "Login successful",
+      token,
+      success: true,
+    });
+  } catch (error) {
+    console.log("error in login ", error);
+    return res.status(500).json({
+      message: "Internal Server Error!",
+      sucess: false,
+    });
+  }
+};
+export default { handleSignup, handleLogin };
