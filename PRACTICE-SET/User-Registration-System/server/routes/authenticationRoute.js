0eb9b0f928d1800b9542ee39b9dc542a5454bfc5
arpita0212 <arpita@pipra.solutions>
2026-04-22 12:58:10 +0530

    added the route for delete profile

--- PRACTICE-SET/User-Registration-System/server/routes/authenticationRoute.js
+++ PRACTICE-SET/User-Registration-System/server/routes/authenticationRoute.js
@@ -3,6 +3,7 @@ import {
   handleSignup,
   handleLogin,
   handleProfileEdit,
+  handleDeleteProfile
 } from "../controllers/user.js";
 
 import { verifyToken } from "../middlewares/authorization.js";
@@ -14,5 +15,7 @@ router.post("/signup", handleSignup);
 router.post("/login", handleLogin);
 
 router.post("/edit-profile", handleProfileEdit, verifyToken);
+
+router.post('/delete-profile',handleDeleteProfile, verifyToken);
 
 export default router;
