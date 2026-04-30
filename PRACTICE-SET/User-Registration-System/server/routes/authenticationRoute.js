31f3ae2b57fa150b37f9fbb51f47f30d4da19630
arpita0212 <arpita@pipra.solutions>
2026-04-30 11:28:43 +0530

    [API] removed the profile related routes

--- PRACTICE-SET/User-Registration-System/server/routes/authenticationRoute.js
+++ PRACTICE-SET/User-Registration-System/server/routes/authenticationRoute.js
@@ -2,8 +2,6 @@ import express from "express";
 import {
   handleSignup,
   handleLogin,
-  handleProfileEdit,
-  handleDeleteProfile
 } from "../controllers/user.js";
 
 import { verifyToken } from "../middlewares/authorization.js";
@@ -13,9 +11,5 @@ const router = express.Router();
 router.post("/signup", handleSignup);
 
 router.post("/login", handleLogin);
-
-router.post("/edit-profile", handleProfileEdit, verifyToken);
-
-router.post('/delete-profile',handleDeleteProfile, verifyToken);
 
 export default router;
