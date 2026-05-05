4037f8c69bbd9b1c2f8cd37fc6688fa182b188c9
arpita0212 <arpita@pipra.solutions>
2026-05-05 17:44:43 +0530

    fixed the middleware

--- PRACTICE-SET/User-Registration-System/server/middlewares/authorization.js
+++ PRACTICE-SET/User-Registration-System/server/middlewares/authorization.js
@@ -1,29 +1,3 @@
-// import jwt from "jsonwebtoken";
-
-// export const verifyToken = (req, res, next) => {
-//   // const token = req.cookies?.accessToken || req.headers.authorization?.split(" ")[1];  console.log("token", token);
-
-//   if (!token) {
-//     return res.status(401).json({
-//       message: "Unauthorized!",
-//       success: false,
-//     });
-//   }
-//   try {
-//     const decodedToken = jwt.verify(token, process.env.SECRETKEY);
-//     req.user = decodedToken;
-//     next();
-//   } catch (error) {
-//     console.log("error", error);
-//     return res.status(500).json({
-//       message: "Internal server error!",
-//       success: false,
-//     });
-//   }
-// };
-
-
-
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
