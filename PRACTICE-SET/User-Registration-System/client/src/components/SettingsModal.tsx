76c15d4d8e0d9aa4056834b5f3f912e1d7d25aa7
arpita0212 <arpita@pipra.solutions>
2026-05-05 17:47:15 +0530

    added the profile button in the modal

--- PRACTICE-SET/User-Registration-System/client/src/components/SettingsModal.tsx
+++ PRACTICE-SET/User-Registration-System/client/src/components/SettingsModal.tsx
@@ -1,9 +1,7 @@
-import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
 import axiosInstance, { endpoints } from '../utils/axios';
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 const style = {
     position: 'absolute',
@@ -13,34 +11,27 @@ const style = {
     width: 150,
     bgcolor: 'background.paper',
     border: '0px',
-    // boxShadow: 24,
     p: 4,
 };
 
-interface UserData {
-    name: string;
-    username: string;
-    email: string;
-    role: string;
-}
 
-export default function BasicModal({ open, handleClose }: any) {
+export default function SettingsModal({ open, handleClose }: any) {
     const navigate = useNavigate();
-    const [snackbar, setSnackbar] = useState({ open: false, message: '', success: false });
-    const [userData, setUserData] = useState<UserData | null>(null);
 
     const handleLogout = async () => {
         try {
-            const resp = await axiosInstance.post(endpoints.logout);
-            setUserData(null);
-            setSnackbar({ open: true, message: "Logged out successfully", success: true });
+            await axiosInstance.post(endpoints.logout);
+            handleClose();
             navigate('/login');
-            console.log(resp);
         }
         catch (error) {
             console.log("error in logout handler", error);
-            setSnackbar({ open: true, message: 'Error occurred while logging out', success: false });
         }
+    }
+
+    const handleEditProfile = () => {
+        handleClose();
+        navigate("/edit-profile");
     }
     return (
         <Modal
@@ -50,7 +41,11 @@ export default function BasicModal({ open, handleClose }: any) {
             aria-describedby="modal-modal-description"
         >
             <Box sx={style}>
-                <Button>
+                <Button onClick={handleEditProfile} sx={{
+                    color: 'black',
+                    fontWeight: '400',
+                    fontSize: '16px',
+                }}>
                     Profile
                 </Button>
                 <Button onClick={handleLogout} sx={{
