816d00c0fc9902a846685aead15baecb9b54423f
arpita0212 <arpita@pipra.solutions>
2026-05-05 17:46:04 +0530

    fixed the controller logic

--- PRACTICE-SET/User-Registration-System/server/controllers/profile.js
+++ PRACTICE-SET/User-Registration-System/server/controllers/profile.js
@@ -5,128 +5,100 @@ import dotenv from "dotenv";
 import multer from "multer";
 import { generateAccessToken, generateRefreshToken } from "../utils/token.js";
 
-dotenv.config();
 
-
-// edit profile
-
-export const handleProfileEdit = async (req, res) => {
+export const handleGetProfileDetails = async (req, res) => {
     try {
-        const token = req.cookies?.accessToken;
+        const userId = req.user.id;
+        console.log(userId);
+        const user = await User.findById(userId).select("-password -refreshToken");
 
-        if (!token) {
+        if (!user) {
             return res.status(401).json({
-                message: "Unauthorized",
-                success: false,
-            });
-        }
-
-        const decoded = jwt.verify(token, process.env.SECRETKEY);
-        const userId = decoded.id;
-
-        const { name, username, email, password, role, profileImage } = req.body;
-
-        let updatedFields = {
-            name,
-            username,
-            email,
-            profileImage,
-        };
-
-        if (password) {
-            updatedFields.password = await bcrypt.hash(password, 12);
-        }
-
-        const updatedUser = await User.findByIdAndUpdate(userId, updatedFields, {
-            new: true,
-        });
-
-        if (!updatedUser) {
-            return res.status(404).json({
                 message: "User not found",
-                status: false,
-            });
-        }
-        return res.status(200).json({
-            message: "Profile updated successfully",
-            User: updatedUser,
-            success: true,
-        });
-    } catch (error) {
-        console.log("error in editing the profile", error);
-        return res.status(200).json({
-            message: "Internal server error!",
-            success: false,
-        });
-    }
-};
-
-
-export const handleDeleteProfile = async (req, res) => {
-    try {
-        const token = req.cookies?.accessToken;
-
-        if (!token) {
-            return res.status(401).json({
-                message: "Unauthorized",
-                status: false
-            });
-        }
-
-        const decoded = jwt.verify(token, process.env.SECRETKEY);
-        const userId = decoded.id;
-
-        const deletedUser = await User.findByIdAndDelete(userId);
-
-        if (!deletedUser) {
-            return res.status(404).json({
-                message: 'User not found!',
-                status: false,
+                success: false
             })
         }
 
-        res.clearCookie('accessToken');
-        res.clearCookie('refreshToken');
-
-        return res.status(200).json({
-            message: 'User successfully delete',
-            success: false
+        res.status(200).json({
+            message: "Profile details fetched successfully",
+            user,
+            success: true,
         })
-
     } catch (error) {
-        console.log("error in deleting the profile", error);
-        return res.status(500).json({
-            message: "Internal server error!",
+        console.log("error in get profile controller", error);
+        res.status(500).json({
+            message: "Internal Server Error!",
             success: false
         })
     }
 }
 
 
-export const handleGetProfileDetails = async (req, res) => {
+export const handleUpdateProfileDetails = async (req, res) => {
     try {
+        const authId = req.cookies?.accessToken;
 
-        const userId = req.user.id;
-        console.log("userId", userId);
-        const user = await User.findById(userId).select("-password -refreshToken");
+        if (!authId) {
+            return res.status(401).json({
+                message: "Unauthorized access!",
+                success: false
+            })
+        }
 
-        if (!user) {
-            return res.status(404).json({
-                message: "User not found!",
-                success: false,
-            });
+        const decoded = jwt.verify(authId, process.env.SECRETKEY);
+        const userId = decoded.id;
+
+        const { name, username, password, profileImage } = req.body;
+        let updatedFields = {
+            name,
+            username,
+            password,
+            profileImage
+        }
+
+        if (password) {
+            updatedFields.password = await User.findByIdAndUpdate(userId, updatedFields, {
+                newUser: true
+            })
+        }
+
+
+    } catch (error) {
+        console.log("error in update profile controller", error);
+        res.status(500).json({
+            message: "Internal Server Error",
+            success: false
+        })
+    }
+}
+
+
+export const handleDeleteUserProfile = async (req, res) => {
+    try {
+        const token = req.headers.authorization;
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const userId = decoded.id;
+
+        const deletedUser = await user.findByIdAndDelete(userId);
+
+        if (!deletedUser) {
+            return res.status(401).json({
+                message: "User not found",
+                status: false
+            })
         }
 
         return res.status(200).json({
-            message: "User data",
-            success: true,
-            user,
-        });
+            message: "Deleted the user successfully",
+            success: true
+        })
+
     } catch (error) {
-        console.log("error in fetching the profile details", error);
-        return res.status(500).json({
-            message: "Internal server error!",
-            success: false,
-        });
+        console.log("error in deleting the data", error);
+        res.status(500).json({
+            message: 'Internal servr error',
+            success: false
+        })
     }
 }
