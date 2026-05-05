71faebeae87290d1d42fc169c8f4d5a293e1ced9
arpita0212 <arpita@pipra.solutions>
2026-05-05 17:46:17 +0530

    fixed the layout issue

--- PRACTICE-SET/User-Registration-System/client/src/layout/MainLayout.tsx
+++ PRACTICE-SET/User-Registration-System/client/src/layout/MainLayout.tsx
@@ -1,16 +1,11 @@
-import { Grid, Typography } from "@mui/material";
-import Dashboard from "../components/Dashboard";
+import { Grid } from "@mui/material";
+import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
-    
-
     return (
         <Grid container spacing={2}>
             <Grid size={{xs: 12, md: 12}}>
-                {/* Header */}
-               
-
-                <Dashboard/>
+                <Outlet/>
             </Grid>
         </Grid>
     )
